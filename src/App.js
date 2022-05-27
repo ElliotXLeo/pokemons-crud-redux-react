6e12c8cd5bc84976c58de4f8cb555834f1101890
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-27 01:03:53 -0500

    layout PokemonsLayout

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {
   Navigate
 } from 'react-router-dom';
 import './App.css';
+import PokemonsLayout from './layouts/PokemonsLayout';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/pokemons" replace={true} />} />
-      <Route path='/pokemons'>
+      <Route path='/pokemons' element={<PokemonsLayout />}>
         <Route path='create' element={<h2>Create Pokémon</h2>} />
         <Route path='update/:id' element={<h2>Update Pokémon</h2>} />
         <Route index element={<h2>Pokémons CRUD Redux React</h2>} />
