6bbdb612f6f8b3d81b1c2620bc21abd58c7a8ef5
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-27 16:06:52 -0500

    Update redux pokemonsReducers CREATE_POKEMON

--- src/redux/reducers/pokemonsReducers.js
+++ src/redux/reducers/pokemonsReducers.js
@@ -1,9 +1,16 @@
+import { CREATE_POKEMON } from "../types/indexTypes";
+
 const initialState = {
   loading: false
 };
 
 const pokemonsReducers = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_POKEMON:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state;
   }
