541c6564cf481dc65f235bc517068ba607966316
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-30 13:06:10 -0500

    Update redux pokemonsReducers READ_POKEMON

--- src/redux/reducers/pokemonsReducers.js
+++ src/redux/reducers/pokemonsReducers.js
@@ -1,8 +1,9 @@
-import { CREATE_POKEMON, CREATE_POKEMON_ERROR, CREATE_POKEMON_SUCCESS, READ_POKEMONS, READ_POKEMONS_ERROR, READ_POKEMONS_SUCCESS } from "../types/indexTypes";
+import { CREATE_POKEMON, CREATE_POKEMON_ERROR, CREATE_POKEMON_SUCCESS, READ_POKEMON, READ_POKEMONS, READ_POKEMONS_ERROR, READ_POKEMONS_SUCCESS, READ_POKEMON_ERROR, READ_POKEMON_SUCCESS } from "../types/indexTypes";
 
 const initialState = {
   loading: false,
   error: {},
+  pokemon: {},
   pokemons: []
 };
 
@@ -40,6 +41,24 @@ const pokemonsReducers = (state = initialState, action) => {
         pokemons: payload
       };
     case READ_POKEMONS_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: payload
+      };
+    case READ_POKEMON:
+      return {
+        ...state,
+        loading: payload
+      };
+    case READ_POKEMON_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: {},
+        pokemons: payload
+      };
+    case READ_POKEMON_ERROR:
       return {
         ...state,
         loading: false,
