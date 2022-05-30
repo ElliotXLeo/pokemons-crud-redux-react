065735b841cae27f627704a64c30874a605d10f7
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-30 13:41:32 -0500

    Update component PokemonsCard

--- src/components/pokemons/PokemonsCard.jsx
+++ src/components/pokemons/PokemonsCard.jsx
@@ -5,26 +5,26 @@ const PokemonsCard = ({ pokemon }) => {
 
   return (
     <div
-      className="flex flex-col items-center gap-4 bg-white rounded-lg shadow shadow-cyan-300 hover:shadow-yellow-400 transition-shadow w-full max-w-xs p-4 dark:bg-zinc-700"
+      className="flex flex-col items-center gap-4 bg-zinc-700 rounded-lg shadow shadow-cyan-300 transition-colors w-full max-w-xs p-4 hover:shadow-yellow-400"
     >
-      <div className="w-32 h-32">
-        <img className="w-full h-full object-cover" src={image.url} alt={name} />
-      </div>
-      <div className="flex flex-col gap-1">
-        <p className="text-zinc-600 text-sm font-thin dark:text-zinc-400">Hp: {hp}</p>
-        <h5 className="text-black text-xl font-medium dark:text-white">{name}</h5>
-        <p className="text-zinc-600 text-sm font-extralight dark:text-zinc-400">{type}</p>
+      <figure className="w-32 h-32">
+        <img src={image.url} alt={name} className="w-full h-full object-cover" />
+      </figure>
+      <div className="flex flex-col gap-1 text-center">
+        <p className="text-zinc-400 text-sm font-thin">Hp: {hp}</p>
+        <h5 className="text-white text-xl font-medium">{name}</h5>
+        <p className="text-zinc-400 text-sm font-extralight">{type}</p>
       </div>
       <div className="flex gap-1">
-        <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{attack}</span>
-        <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{special}</span>
+        <span className="bg-zinc-800 rounded-xl text-white text-sm px-3 py-1">{attack}</span>
+        <span className="bg-zinc-800 rounded-xl text-white text-sm px-3 py-1">{special}</span>
       </div>
       <div className="flex gap-2">
-        <Link to={`${_id}`} className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-white">
+        <Link to={`${_id}`} className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 transition-colors hover:bg-white">
           Editar
         </Link>
         <button
-          className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-rose-600"
+          className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 transition-colors hover:bg-rose-600"
         >
           Eliminar
         </button>
