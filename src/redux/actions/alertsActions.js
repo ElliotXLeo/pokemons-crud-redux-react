ee1287e816c47c89a068fc0dbb524e5c0937f638
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-31 15:50:33 -0500

    Update redux alertsActions HIDE_ALERT

--- src/redux/actions/alertsActions.js
+++ src/redux/actions/alertsActions.js
@@ -1,12 +1,20 @@
-import { SHOW_ALERT } from "../types/alertsTypes";
+import { HIDE_ALERT, SHOW_ALERT } from "../types/alertsTypes";
 
 const showAlertAction = (alert) => ({
   type: SHOW_ALERT,
   payload: alert
 });
 
+const hideAlertAction = () => ({
+  type: HIDE_ALERT,
+  payload: {}
+});
+
 export const showAlert = (alert) => {
   return ((dispatch) => {
     dispatch(showAlertAction(alert));
+    setTimeout(() => {
+      dispatch(hideAlertAction());
+    }, 5000);
   });
 };
