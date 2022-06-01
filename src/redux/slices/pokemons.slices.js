801a1149b22b20d51f9e9246023123bd33e605b3
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-01 17:51:20 -0500

    redux pokemons.slices fetchCreatePokemon

---
+++ src/redux/slices/pokemons.slices.js
@@ -0,0 +1,84 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axiosInstance from "../../config/axiosInstance";
+import { showToast } from "../../utils/sweetalert";
+
+const initialState = {
+  loading: false,
+  error: {},
+  pokemon: {},
+  pokemons: []
+};
+
+const pokemonsSlices = createSlice({
+  name: 'pokemons',
+  initialState,
+  reducers: {
+    fetchCreatePokemonRequest(state, action) {
+      return {
+        ...state,
+        loading: action.payload
+      };
+    },
+    fetchCreatePokemonSuccess(state, action) {
+      return {
+        ...state,
+        loading: false,
+        error: {},
+        pokemons: [...state.pokemons, action.payload]
+      };
+    },
+    fetchCreatePokemonError(state, action) {
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    }
+  }
+});
+
+export const {
+  fetchCreatePokemonRequest,
+  fetchCreatePokemonSuccess,
+  fetchCreatePokemonError
+} = pokemonsSlices.actions;
+
+let alertTimeId = 0;
+
+const showError = (error, pokemonErrorAction) => {
+  return (async (dispatch) => {
+    clearTimeout(alertTimeId);
+    showToast('error', 'Error');
+    dispatch(pokemonErrorAction(error));
+    alertTimeId = setTimeout(() => {
+      dispatch(pokemonErrorAction({}));
+    }, 5000);
+  });
+};
+
+export const fetchCreatePokemon = (pokemon) => {
+  return (async (dispatch) => {
+    dispatch(fetchCreatePokemonRequest());
+    try {
+      const form = new FormData();
+      for (const key in pokemon) {
+        form.append(key, pokemon[key]);
+      }
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        data: form,
+        url: `/pokemons`
+      };
+      const { data } = await axiosInstance(options);
+      dispatch(fetchCreatePokemonSuccess(data));
+      showToast('success', 'Creado');
+    } catch (error) {
+      dispatch(showError(error, fetchCreatePokemonError));
+    }
+  });
+};
+
+export default pokemonsSlices.reducer;
