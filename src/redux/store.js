a1c523b9810d6f5484aef1328d529c4d4430906a
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-02 14:11:51 -0500

    Update redux store to @reduxjs/toolkit alerts showAlert

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,22 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import alertsSlices from "./slices/alerts.slices";
 import pokemonsSlices from "./slices/pokemons.slices";
 
 const store = configureStore({
   reducer: {
-    pokemons: pokemonsSlices
+    pokemons: pokemonsSlices,
+    alerts: alertsSlices
   }
 });
 
 export default store;
-
-// import { applyMiddleware, combineReducers, createStore } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import thunk from "redux-thunk";
-// import alertsReducers from "./reducers/alertsReducers";
-
-// const rootReducer = combineReducers({
-//   alertsReducers
-// });
-
-// const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-// export default store;
