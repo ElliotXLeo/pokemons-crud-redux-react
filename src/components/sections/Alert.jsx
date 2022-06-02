e2fbe9b4c7e46083eea1e01750be41a1f4dd707e
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-02 14:36:05 -0500

    Update component Alert

--- src/components/sections/Alert.jsx
+++ src/components/sections/Alert.jsx
@@ -1,10 +1,10 @@
 const Alert = ({ alert }) => {
   const { name, message } = alert;
   return (
-    <section className="py-4">
-      <div className="flex justify-center container mx-auto px-8 md:px-4">
-        <div className={`flex items-center gap-2 bg-cyan-300 rounded text-black p-2 ${alert.name === 'AxiosError' && 'bg-rose-500 text-white' }`} role="alert">
-          <h3 className="bg-white rounded px-2 py-1 font-bold">{name}</h3>
+    <section className="fixed z-40 left-1/2 top-8 -translate-x-1/2">
+      <div className="flex justify-center">
+        <div className={`flex items-center gap-2 rounded  p-2 ${alert.name !== 'AxiosError' ? 'bg-cyan-300/75 text-black' : 'bg-rose-500/75 text-white'}`} role="alert">
+          <h3 className="bg-white text-black rounded px-2 py-1 font-bold">{name}</h3>
           <p className="font-semibold">{alert.response?.data.message} [{message}]</p>
         </div>
       </div>
